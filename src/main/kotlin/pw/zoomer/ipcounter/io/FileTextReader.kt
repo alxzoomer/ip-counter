1f506d56904ba6f8fb6cd63cc46506da4e6d57ef
Alexey Nikitin <alxzoomer@gmail.com>
2021-01-02 03:17:21 +0400

    Add comment for FileTextReader

--- src/main/kotlin/pw/zoomer/ipcounter/io/FileTextReader.kt
+++ src/main/kotlin/pw/zoomer/ipcounter/io/FileTextReader.kt
@@ -2,9 +2,17 @@ package pw.zoomer.ipcounter.io
 
 import java.io.File
 
-class FileTextReader(val pathName: String): TextReader {
+/**
+ * Read text file from beginning till the end line by line.
+ * @param pathName Full path to text file.
+ */
+class FileTextReader(pathName: String): TextReader {
     private var file: File = File(pathName)
 
+    /**
+     * Read text file and call action.
+     * @param action Call action for each line.
+     */
     override fun forEachLine(action: (line: String) -> Unit) {
         file.forEachLine(action = action)
     }
