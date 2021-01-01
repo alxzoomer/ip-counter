c35fc302b3f437ca9cca79dec73f75d166edf0b0
Alexey Nikitin <alxzoomer@gmail.com>
2021-01-02 03:35:27 +0400

    Add single threader counter

---
+++ src/main/kotlin/pw/zoomer/ipcounter/jobs/SingleThreadedIpCounter.kt
@@ -0,0 +1,35 @@
+package pw.zoomer.ipcounter.jobs
+
+import pw.zoomer.ipcounter.data.IpStore
+import pw.zoomer.ipcounter.io.TextReader
+import pw.zoomer.ipcounter.log.ILogger
+import pw.zoomer.ipcounter.net.IpV4Parser
+
+/**
+ * Class reads IP addresses from text reader source line by line and writes parsed IP's to IP store.
+ * @param reader TextReader instance as IP source.
+ * @param ipStore IpStore destination IP store.
+ * @param logger ILogger logger instance to write parsing errors.
+ */
+class SingleThreadedIpCounter(
+    private val reader: TextReader,
+    private val ipStore: IpStore,
+    private val logger: ILogger
+) {
+    /**
+     * Get count of stored IPs.
+     */
+    val count
+        get() = ipStore.count
+
+    /**
+     * Start IP reading and parsing.
+     */
+    fun start() {
+        reader.forEachLine {
+            if (IpV4Parser.toLongOrNull(it)?.let { ip -> ipStore.add(ip) } == null) {
+                logger.error("Incorrect IP address $it")
+            }
+        }
+    }
+}
