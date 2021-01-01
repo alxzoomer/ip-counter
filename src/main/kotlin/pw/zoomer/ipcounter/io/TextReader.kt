7492a5721fca85c91cbd39384a3e9d03248b7f56
Alexey Nikitin <alxzoomer@gmail.com>
2021-01-02 03:12:00 +0400

    Add comment for TextReader interface

--- src/main/kotlin/pw/zoomer/ipcounter/io/TextReader.kt
+++ src/main/kotlin/pw/zoomer/ipcounter/io/TextReader.kt
@@ -1,5 +1,11 @@
 package pw.zoomer.ipcounter.io
 
+/**
+ * Interface provides access to text source line by line.
+ */
 interface TextReader {
+    /**
+     * Reads text source line by line and calls action for each line.
+     */
     fun forEachLine(action: (line: String) -> Unit)
 }
