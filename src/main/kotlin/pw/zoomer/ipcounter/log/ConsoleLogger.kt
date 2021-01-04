0977a9be7a777d8e8fec8e4a2716a57b7f735e66
Alexey Nikitin <alxzoomer@gmail.com>
2021-01-05 03:28:03 +0400

    Update code documentation

--- src/main/kotlin/pw/zoomer/ipcounter/log/ConsoleLogger.kt
+++ src/main/kotlin/pw/zoomer/ipcounter/log/ConsoleLogger.kt
@@ -1,5 +1,8 @@
 package pw.zoomer.ipcounter.log
 
+/**
+ * Write logs to standard output.
+ */
 class ConsoleLogger : Logger {
     override fun error(message: String) {
         println("ERROR: $message")
