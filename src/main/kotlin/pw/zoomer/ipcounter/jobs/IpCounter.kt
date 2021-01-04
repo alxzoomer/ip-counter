b77a80bcd410b41398aa871f91e3322df9ea5fce
Alexey Nikitin <alxzoomer@gmail.com>
2021-01-05 00:25:33 +0400

    Add progress information for IpCounter

--- src/main/kotlin/pw/zoomer/ipcounter/jobs/IpCounter.kt
+++ src/main/kotlin/pw/zoomer/ipcounter/jobs/IpCounter.kt
@@ -4,6 +4,8 @@ import pw.zoomer.ipcounter.data.IpStore
 import pw.zoomer.ipcounter.io.TextReader
 import pw.zoomer.ipcounter.log.Logger
 import pw.zoomer.ipcounter.net.QuickIpV4Parser
+import java.text.NumberFormat
+import java.util.*
 
 /**
  * Class reads IP addresses from text reader source line by line and writes parsed IP's to IP store.
@@ -26,9 +28,15 @@ class IpCounter(
      * Start IP reading and parsing.
      */
     fun start() {
+        var i = 0L
+        val formatter = NumberFormat.getNumberInstance(Locale.US)
         reader.forEachLine {
             if (QuickIpV4Parser.toLongOrNull(it)?.let { ip -> ipStore.add(ip) } == null) {
                 logger.error("Incorrect IP address $it")
+            }
+            i++
+            if (i % 10_000_000 == 0L) {
+                logger.info("Processed ${formatter.format(i)} lines")
             }
         }
     }
